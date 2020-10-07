9124c7d1c1b61ef4e6711dcd0113cdd779775357
Luke Saunders <luke.saunders@gmail.com>
2020-10-07 20:53:03 +0200

    init

---
+++ knexfile.mjs
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'midgard',
+      user:     'midgard',
+      password: 'password',
+      timezone: 'UTC',
+    },
+    // debug: true    
+  },
+
+};
